c569683fa903695a254630b2a258db8b8c5eab16
uhyo <uhyo@uhy.ooo>
2021-03-12 19:36:02 +0900

    feat(ccdl): byteString.length

--- packages/ccdl/src/syntax/byteString.test.ts
+++ packages/ccdl/src/syntax/byteString.test.ts
@@ -1,8 +1,10 @@
+import { PassThrough } from "node:stream";
 import { bufferConcat } from "../utils/test/bufConcat.js";
 import {
   asyncBufferToStream,
   bufferToStream,
 } from "../utils/test/bufferToStream.js";
+import { receiveToBuffer } from "../utils/test/receiveToBuffer.js";
 import { writeToBuffer } from "../utils/test/writeToBuffer.js";
 import { byteString } from "./byteString.js";
 
@@ -95,6 +97,101 @@ describe("byteString", () => {
     it("failure (wrong tag)", async () => {
       const l = bufferToStream(bufferConcat(0b100_00000));
       await expect(byteString.read(l)).rejects.toThrow(
+        "Expected major type 2, received 4"
+      );
+    });
+  });
+});
+
+describe("byteString.length", () => {
+  describe("encode", () => {
+    const syntax = byteString.length(3, 5);
+    it("encode short string", async () => {
+      const res = await writeToBuffer(async (l) => {
+        await syntax.encode(l, "hello");
+      });
+      expect(res).toEqual(bufferConcat(0b010_00101, "hello"));
+    });
+    it("encode UTF-8", async () => {
+      const res = await writeToBuffer(async (l) => {
+        await syntax.encode(l, "😃");
+      });
+      expect(res).toEqual(bufferConcat(0b010_00100, "😃"));
+    });
+    it("encode 65536 bytes", async () => {
+      const syntax = byteString.length(65536, 65536);
+      const res = await writeToBuffer(async (l) => {
+        await syntax.encode(l, "a".repeat(65536));
+      });
+      expect(res).toEqual(
+        bufferConcat(0b010_11010, 0, 1, 0, 0, "a".repeat(65536))
+      );
+    });
+    it("failure (too short)", async () => {
+      const l = new PassThrough();
+      await expect(syntax.encode(l, "a")).rejects.toThrow(
+        "Length of given data is not in range [3, 5]"
+      );
+    });
+    it("failure (too long)", async () => {
+      const l = new PassThrough();
+      await expect(syntax.encode(l, "Hello, world!")).rejects.toThrow(
+        "Length of given data is not in range [3, 5]"
+      );
+    });
+  });
+  describe("count", () => {
+    const syntax = byteString.length(3, 5);
+    it("count short string", () => {
+      expect(syntax.count("Pika!")).toEqual(1 + 5);
+    });
+    it("count UTF-8", () => {
+      expect(syntax.count("富士山🗻")).toEqual(1 + 3 * 3 + 4);
+    });
+    it("count 100 bytes", () => {
+      expect(syntax.count("🙂".repeat(25))).toEqual(2 + 4 * 25);
+    });
+    it("count 1024 bytes", () => {
+      expect(syntax.count("z".repeat(1024))).toEqual(3 + 1024);
+    });
+    it("count 100000 bytes", () => {
+      expect(syntax.count("a".repeat(100_000))).toEqual(5 + 100_000);
+    });
+  });
+  describe("decode", () => {
+    const syntax = byteString.length(3, 5);
+    it("successful", async () => {
+      const buf = bufferConcat(0b010_00100, "pika");
+      const l = bufferToStream(buf);
+      const result = await byteString.read(l);
+      expect(result).toEqual({
+        bytesRead: 5,
+        data: Buffer.from("pika"),
+      });
+    });
+    it("failure (too short)", async () => {
+      const l = bufferToStream(0b010_00001, "a");
+      await expect(syntax.read(l)).rejects.toThrow(
+        "Expected byteString length to be in [3, 5], received 1"
+      );
+      // only header is drained from buffer
+      expect(await receiveToBuffer(l)).toEqual(Buffer.from("a"));
+    });
+    it("failure (too long)", async () => {
+      const l = bufferToStream(0b010_11000, 30, "x".repeat(30));
+      await expect(syntax.read(l)).rejects.toThrow(
+        "Expected byteString length to be in [3, 5], received 30"
+      );
+      // only header is drained from buffer
+      expect(await receiveToBuffer(l)).toEqual(Buffer.from("x".repeat(30)));
+    });
+    it("failure (insufficient data)", async () => {
+      const [l] = asyncBufferToStream();
+      await expect(syntax.read(l)).rejects.toThrow("Unexpected end of input");
+    });
+    it("failure (wrong tag)", async () => {
+      const l = bufferToStream(bufferConcat(0b100_00000));
+      await expect(syntax.read(l)).rejects.toThrow(
         "Expected major type 2, received 4"
       );
     });
--- packages/ccdl/src/syntax/byteString.ts
+++ packages/ccdl/src/syntax/byteString.ts
@@ -1,10 +1,23 @@
 import { assertMajorType } from "../error/assertion.js";
+import { CCDLSyntaxError } from "../error/CCDLSyntaxError.js";
 import { readBytes } from "../stream/readBytes.js";
 import { CCDLSyntax } from "./internal/CCDLSyntax.js";
 import { countHead, readHead, writeHead } from "./internal/head.js";
 import { majorTypes } from "./internal/MajorType.js";
 
-export const byteString: CCDLSyntax<Buffer, string | Buffer> = {
+export type ByteStringSyntax = CCDLSyntax<Buffer, string | Buffer> & {
+  /**
+   * Create a ByteString syntax with limited length.
+   */
+  length(min: number, max: number): ByteStringLengthSyntax;
+};
+
+export type ByteStringLengthSyntax = CCDLSyntax<Buffer, string | Buffer> & {
+  min: number;
+  max: number;
+};
+
+export const byteString: ByteStringSyntax = {
   async encode(stream, data) {
     const length = Buffer.byteLength(data);
     // write length.
@@ -27,4 +40,39 @@ export const byteString: CCDLSyntax<Buffer, string | Buffer> = {
     };
   },
   rootSize: 1,
+  length,
 };
+
+function length(min: number, max: number): ByteStringLengthSyntax {
+  return {
+    min,
+    max,
+    async encode(stream, data) {
+      const byteLength = Buffer.byteLength(data);
+      if (byteLength < this.min || this.max < byteLength) {
+        throw new Error(
+          `Length of given data is not in range [${this.min}, ${this.max}]`
+        );
+      }
+      return byteString.encode(stream, data);
+    },
+    count(data) {
+      return byteString.count(data);
+    },
+    async read(stream) {
+      const { majorType, additionalInfo, bytesRead } = await readHead(stream);
+      assertMajorType(majorTypes.byteString, majorType);
+      if (additionalInfo < this.min || this.max < additionalInfo) {
+        throw new CCDLSyntaxError(
+          `Expected byteString length to be in [${this.min}, ${this.max}], received ${additionalInfo}`
+        );
+      }
+      const buf = await readBytes(stream, additionalInfo);
+      return {
+        bytesRead: bytesRead + buf.length,
+        data: buf,
+      };
+    },
+    rootSize: 1,
+  };
+}
